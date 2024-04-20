6599a5740a18209fb726ea0c195efc48c6ba5f26
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2024-04-20 18:40:36 -0300

    Configuração do ESLint

--- packages/tokens/src/font-sizes.ts
+++ packages/tokens/src/font-sizes.ts
@@ -2,7 +2,7 @@ export const fontSizes = {
   xxs: '0.625rem',
   xs: '0.75rem',
   sm: '0.875rem',
-  md: '1rem', //16px
+  md: '1rem', // 16px
   lg: '1.125rem',
   xl: '1.25rem',
   '2xl': '1.5rem',
