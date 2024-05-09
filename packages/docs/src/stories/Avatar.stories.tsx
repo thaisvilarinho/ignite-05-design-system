df6c003f13ad5bcbe0347ccbc1617e5cd0e4760f
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2024-05-08 21:11:32 -0300

    Componente: Button

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/thaisvilarinho.png',
+    alt: 'Thaís Vilarinho',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
