f371884d3ce66ec39fbfdafd79ec8a920f529af9
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2024-05-10 19:35:28 -0300

    adicionando subdiretorio storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -35,5 +35,11 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+    return config
+  }
 };
 export default config;
